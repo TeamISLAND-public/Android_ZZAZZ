1637dc653bdf2d7e65f1e79deac42305cafa94e0
kdh0324 <kdh0324@postech.ac.kr>
2020-04-14 16:59:02 +0900

    Develop video play button

--- app/src/main/java/com/example/ui_video/MainActivity.kt
+++ app/src/main/java/com/example/ui_video/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.ui_video
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.MediaController
 import android.widget.VideoView
 
@@ -13,18 +15,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.ui_video)
 
         val video = findViewById<VideoView>(R.id.video_view)
+        val playBtn = findViewById<Button>(R.id.play_button)
+        var checkFinish = false
 
         val controller = MediaController(this)
+        controller.hide()
+
         video.setMediaController(controller)
 
         video.requestFocus()
         video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test_5s));
 
+        video.start()
         video.postDelayed(Runnable {
             run {
                 controller.show(0)
+                video.pause()
             }
         }, 100)
+
+        playBtn.setOnClickListener {
+            if (video.isPlaying) {
+                video.pause()
+                playBtn.text = "Play"
+            } else {
+                if (checkFinish) {
+                    video.seekTo(0)
+                    checkFinish = false
+                }
+                video.start()
+                playBtn.text = "Pause"
+            }
+
+            video.setOnCompletionListener {
+                video.pause()
+                playBtn.text = "Play"
+                checkFinish = true
+            }
+        }
     }
 }
 
