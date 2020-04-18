0fa047445f15a4c1dc8d3434f18440fa7a140299
DelVel <56226851+DelVel@users.noreply.github.com>
2020-04-18 22:07:04 +0900

    Gradle & Kotlin update, activity renamed.
    Signing test.

--- app/src/main/java/com/example/myapplication/TrimmingActivity.kt
+++ app/src/main/java/com/example/myapplication/TrimmingActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_after_selection.*
 
-class AfterSelectionActivity : AppCompatActivity() {
+class TrimmingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
