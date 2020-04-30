c443ba9841f8e3abc7d93748b59689b113da31d9
DelVel <56226851+DelVel@users.noreply.github.com>
2020-04-30 12:39:07 +0900

    Basic setup

--- app/src/main/java/com/example/myapplication/TrimmingActivity.kt
+++ app/src/main/java/com/example/myapplication/TrimmingActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_after_selection.*
 
@@ -11,6 +12,11 @@ class TrimmingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_selection)
         val video_uri = intent.getParcelableExtra<Uri>(getString(R.string.selected_video_uri))
+        if (video_uri == null) {
+            Toast.makeText(this, "Invalid video URI; Contact the developer.", Toast.LENGTH_LONG)
+                .show()
+            return
+        }
         videoView.setVideoURI(video_uri)
         videoView.start()
         videoView.setOnCompletionListener { videoView.start() }
