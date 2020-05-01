c491c87b60d6130515c66ebfad3c5d0f600b1301
DelVel <56226851+DelVel@users.noreply.github.com>
2020-05-01 22:41:19 +0900

    Package updated.

--- app/src/main/java/com/teamisland/zzazz/AfterSelectionActivity.kt
+++ app/src/main/java/com/teamisland/zzazz/AfterSelectionActivity.kt
@@ -1,10 +1,13 @@
-package com.example.myapplication
+package com.teamisland.zzazz
 
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_after_selection.*
 
+/**
+ * Activity for video trimming.
+ */
 class AfterSelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
