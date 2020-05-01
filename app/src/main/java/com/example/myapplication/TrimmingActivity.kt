4872c7d9815c1307cc565a1094ba58137683b68c
DelVel <56226851+DelVel@users.noreply.github.com>
2020-05-01 12:00:02 +0900

    RangeSlider added.

--- app/src/main/java/com/example/myapplication/TrimmingActivity.kt
+++ app/src/main/java/com/example/myapplication/TrimmingActivity.kt
@@ -2,11 +2,20 @@ package com.example.myapplication
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_after_selection.*
+import org.florescu.android.rangeseekbar.RangeSeekBar
 
 class TrimmingActivity : AppCompatActivity() {
+
+    private fun barUpdateHandler(a: RangeSeekBar<Int>) {
+        val i = a.selectedMinValue * 1000
+        Log.d("@@@", "Milsec${i}")
+        videoView.seekTo(i)
+        videoView.start()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +26,13 @@ class TrimmingActivity : AppCompatActivity() {
                 .show()
             return
         }
+        val rangeBar2 = rangeBar as RangeSeekBar<Int>
         videoView.setVideoURI(video_uri)
+        videoView.setOnCompletionListener { }
         videoView.start()
         videoView.setOnCompletionListener { videoView.start() }
+        rangeBar2.setOnRangeSeekBarChangeListener { a: RangeSeekBar<Int>, _: Number, _: Number ->
+            barUpdateHandler(a)
+        }
     }
 }
