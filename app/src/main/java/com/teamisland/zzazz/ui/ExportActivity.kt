f0d2d86f7a7ec6613601eb9e3b0b31659d0200c6
kdh0324 <kdh0324@postech.ac.kr>
2020-05-19 19:40:50 +0900

    Develop function to share with instagram & kakaotalk

--- app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
+++ app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
@@ -1,7 +1,10 @@
 package com.teamisland.zzazz.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.teamisland.zzazz.R
 
 class ExportActivity : AppCompatActivity() {
@@ -9,5 +12,25 @@ class ExportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_export)
+
+        val instagram = findViewById<Button>(R.id.share_instagram)
+        val kakaotalk = findViewById<Button>(R.id.share_kakaotalk)
+        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.test_5s)
+
+        instagram.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "video/*"
+            intent.putExtra(Intent.EXTRA_STREAM, uri)
+            intent.setPackage("com.instagram.android")
+            startActivity(intent)
+        }
+
+        kakaotalk.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "video/*"
+            intent.putExtra(Intent.EXTRA_STREAM, uri)
+            intent.setPackage("com.kakao.talk")
+            startActivity(intent)
+        }
     }
 }
