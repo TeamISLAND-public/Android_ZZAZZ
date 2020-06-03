7f2c4f8c05a20e7b337a58b38979a24ab92b8027
kdh0324 <55946729+kdh0324@users.noreply.github.com>
2020-06-03 14:18:26 +0900

    App ui export exporting (#34)
    * commit for checkout
    
    * finish exporting function
    
    * change image of kakaotalk & instagram and set opacity
    
    * set percentage of dialog
    
    * Update app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
    
    suggestion by @Delvel
    
    Co-authored-by: Taegyu Park <56226851+DelVel@users.noreply.github.com>

--- app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
+++ app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
@@ -1,43 +1,59 @@
 package com.teamisland.zzazz.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.view.Gravity
 import android.view.Window
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.teamisland.zzazz.R
 import com.teamisland.zzazz.utils.VideoIntent
 import kotlinx.android.synthetic.main.activity_export.*
 import kotlinx.android.synthetic.main.export_dialog.*
-import kotlinx.android.synthetic.main.finish_toast.*
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.properties.Delegates
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class ExportActivity : AppCompatActivity() {
 
     private lateinit var uri: String
     private var duration by Delegates.notNull<Int>()
 
+    @SuppressLint("SetTextI18n", "SimpleDateFormat", "InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_export)
 
-
         val value = intent.getParcelableExtra<VideoIntent>("value")
-//        duration = value.duration
 //        uri = value.uri.toString()
-        duration = 0
-        uri = ""
+        uri = "android.resource://$packageName/" + R.raw.test_5s
 
         videoInit()
+
+        // set translucent the image when they are not installed
+        if (!isInstall("com.instagram.android")) {
+            share_instagram.alpha = 0.5F
+        }
+        if (!isInstall("com.kakaotalk.android")) {
+            share_kakaotalk.alpha = 0.5F
+        }
 
         buttonToExport.setOnClickListener {
             val dialog = Dialog(this)
@@ -51,30 +67,63 @@ class ExportActivity : AppCompatActivity() {
             window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             window?.setGravity(Gravity.CENTER)
 
-            var percentage: Int = 0
-            dialog.progress_text.text = "$percentage%"
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    1
+                );
+            }
+
+            val input =
+                contentResolver.openInputStream(Uri.parse(uri))
+            val dirString = Environment.getExternalStorageDirectory().toString() + "/ZZAZZ"
+            val dir = File(dirString)
+            if (!dir.exists()) {
+                dir.mkdirs()
+            }
+            val time = System.currentTimeMillis()
+            val date = Date(time)
+            val nameFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
+            val filename = nameFormat.format(date)
+            val file = "$dirString/$filename.mp4"
+            val output = FileOutputStream(File(file))
+            val data = ByteArray(1024)
+            val len = input!!.available()
+            var total = 0
+            var count: Int
+            dialog.progress_text.text = "$total%"
             val handler = Handler()
 
             Thread(Runnable {
-                while (percentage<100) {
-                    percentage++
-                    Thread.sleep(100)
+                count = input.read(data)
+                while (count != -1) {
+                    total += count
 
                     handler.post {
-                        dialog.export_progress.progress = percentage
-                        dialog.progress_text.text = "$percentage%"
-                        if (percentage == 100) {
-                            dialog.dismiss()
-                            val layout = layoutInflater.inflate(R.layout.finish_toast, finish)
-                            val toast = Toast(this)
-                            toast.setGravity(Gravity.CENTER, 0, 0)
-                            toast.duration = Toast.LENGTH_SHORT
-                            toast.view = layout
-                            toast.show()
-                        }
+                        dialog.progress_text.text = (total / len * 100).toString() + "%"
                     }
+
+                    output.write(data, 0, count)
+                    count = input.read(data)
                 }
+                output.flush()
+                output.close()
+                input.close()
+                dialog.dismiss()
             }).start()
+
+            val toast = Toast(this)
+            val layout = layoutInflater.inflate(R.layout.finish_toast, null)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.view = layout
+            toast.show()
         }
 
         share_instagram.setOnClickListener {
@@ -102,6 +151,7 @@ class ExportActivity : AppCompatActivity() {
     private fun videoInit() {
         preview.setMediaController(null)
         preview.setVideoURI(Uri.parse(uri))
+        duration = preview.duration
 
         preview.setOnPreparedListener {
             preview_progress.max = duration
@@ -169,5 +219,19 @@ class ExportActivity : AppCompatActivity() {
             preview_play.text = setTextPlay()
             position = 0
         }
+    }
+
+    private fun isInstall(packageName: String): Boolean {
+        val intent = packageManager.getLaunchIntentForPackage(packageName)
+        return intent != null
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) 
+                return
     }
 }
