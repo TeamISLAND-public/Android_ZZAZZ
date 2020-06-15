adfca6593ffa95e453f763c7f3aeb201f9100bd2
kdh0324 <55946729+kdh0324@users.noreply.github.com>
2020-06-16 05:11:11 +0900

    App ui export savebutton (#40)
    * Implement Done Button
    
    * Implement Back Button
    
    * fix little bit, but does not affect excecution
    
    * get vector image of save button
    
    * fix some strings & set save text
    
    * fix UI & set diveder, save button, text
    
    * get permission of internet to load ad
    
    * fix saving function to load in export activity & fix bugs in adview and playbutton
    
    * fix ui

--- app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
+++ app/src/main/java/com/teamisland/zzazz/ui/ExportActivity.kt
@@ -24,6 +24,7 @@ import android.widget.SeekBar
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.teamisland.zzazz.R
@@ -43,8 +44,6 @@ class ExportActivity : AppCompatActivity() {
     private lateinit var uri: String
     private var duration: Int = 0
 
-    @SuppressLint("SimpleDateFormat", "SetTextI18n", "InflateParams")
-    @Suppress("BlockingMethodInNonBlockingContext")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_export)
@@ -54,22 +53,6 @@ class ExportActivity : AppCompatActivity() {
         //This is for test
         uri = "android.resource://$packageName/" + R.raw.test
 
-        var checkStop = false
-
-        //Check permission
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                1
-            )
-        }
-
         // set translucent the image when they are not installed
         if (!isInstall("com.instagram.android")) {
             share_instagram.alpha = 0.5F
@@ -78,94 +61,11 @@ class ExportActivity : AppCompatActivity() {
             share_kakaotalk.alpha = 0.5F
         }
 
-        val dialog = Dialog(this)
-        dialog.setCancelable(false)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(R.layout.export_dialog)
-        dialog.create()
-        dialog.show()
-        val window = dialog.window
-
-        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        window?.setGravity(Gravity.CENTER)
-
-        val input = contentResolver.openInputStream(Uri.parse(uri))
-
-        //Make file directory for saving the video
-        val dirString = Environment.getExternalStorageDirectory().toString() + "/ZZAZZ"
-        val dir = File(dirString)
-        if (!dir.exists()) {
-            dir.mkdirs()
-        }
-
-        //Video name is depended by time
-        val time = System.currentTimeMillis()
-        val date = Date(time)
-        val nameFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
-        val filename = nameFormat.format(date)
-        val file = "$dirString/$filename.mp4"
-        val output = FileOutputStream(File(file))
-
-        val data = ByteArray(1024)
-        val len = input!!.available()
-        var total = 0
-        var count: Int
-        val handler = Handler()
-        dialog.progress_text.text = "$total%"
-
-        //Use coroutine for exporting
-        val exporting = GlobalScope.launch {
-            count = try {
-                input.read(data)
-            } catch (e: Exception) {
-                -1
-            }
-            while (count != -1) {
-                total += count
-
-                handler.post {
-                    dialog.progress_text.text =
-                        (total.toDouble() / len * 100).toInt().toString() + "%"
-                }
-
-                try {
-                    output.write(data, 0, count)
-                    count = input.read(data)
-                } catch (e: Exception) {
-                    checkStop = true
-                    break
-                }
-            }
-
-            if (!checkStop) {
-                input.close()
-                output.flush()
-                output.close()
-                dialog.dismiss()
-
-                handler.post {
-                    val toast = Toast(this@ExportActivity)
-                    val layout = layoutInflater.inflate(R.layout.finish_toast, null)
-                    toast.setGravity(Gravity.CENTER, 0, 0)
-                    toast.view = layout
-                    toast.show()
-
-                    preview.seekTo(0)
-                    preview.start()
-                }
-            }
-        }
-
-        dialog.export_stop.setOnClickListener {
-            input.close()
-            output.close()
-            dialog.dismiss()
-            File(file).delete()
-            exporting.cancel()
-            finish()
-        }
-
         videoInit()
+
+        save.setOnClickListener {
+            videoSave()
+        }
 
         share_instagram.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
@@ -183,15 +83,13 @@ class ExportActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // test ad
-        MobileAds.initialize(this) {}
-
         //This is for test device which is the emulator
         //We have to remove the code which sets test device id before releasing application
-        val testDeviceIds = mutableListOf("33BE2250B43518CCDA7DE426D04EE231")
-        val configuration =
-            RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
+        val testDeviceIds = listOf("33BE2250B43518CCDA7DE426D04EE231")
+        val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
         MobileAds.setRequestConfiguration(configuration)
+        MobileAds.initialize(this)
+        adView.loadAd(AdRequest.Builder().build())
     }
 
     @SuppressLint("SetTextI18n")
@@ -219,6 +117,25 @@ class ExportActivity : AppCompatActivity() {
     }
 
     private fun videoStart() {
+        preview.seekTo(0)
+        preview.start()
+
+        val fadeOut = AnimationUtils.loadAnimation(this@ExportActivity, R.anim.fade_out)
+        fadeOut.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            // button needs to be vanished
+            override fun onAnimationEnd(animation: Animation?) {
+                preview_play.visibility = View.GONE
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
+        preview_play.startAnimation(fadeOut)
+
         //This is for video is end
         //When video is end, preview will start from first
         var end = false
@@ -228,7 +145,8 @@ class ExportActivity : AppCompatActivity() {
         done_export.setOnClickListener {
             done = true
             val intent = Intent(this, IntroActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.flags =
+                Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
 
@@ -253,7 +171,6 @@ class ExportActivity : AppCompatActivity() {
             }).start()
         }
 
-        val fadeOut = AnimationUtils.loadAnimation(this@ExportActivity, R.anim.fade_out)
         preview.setOnClickListener {
             preview_play.visibility = View.VISIBLE
 
@@ -343,6 +260,109 @@ class ExportActivity : AppCompatActivity() {
             preview.pause()
             preview_play.setImageDrawable(getDrawable(R.drawable.preview_play))
             end = true
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    @SuppressLint("SimpleDateFormat", "SetTextI18n", "InflateParams")
+    private fun videoSave() {
+        var checkStop = false
+
+        //Check permission
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                1
+            )
+        }
+
+        val dialog = Dialog(this)
+        dialog.setCancelable(false)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.export_dialog)
+        dialog.create()
+        dialog.show()
+        val window = dialog.window
+
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window?.setGravity(Gravity.CENTER)
+
+        val input = contentResolver.openInputStream(Uri.parse(uri))
+
+        //Make file directory for saving the video
+        val dirString = Environment.getExternalStorageDirectory().toString() + "/ZZAZZ"
+        val dir = File(dirString)
+        if (!dir.exists()) {
+            dir.mkdirs()
+        }
+
+        //Video name is depended by time
+        val time = System.currentTimeMillis()
+        val date = Date(time)
+        val nameFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = nameFormat.format(date)
+        val file = "$dirString/$filename.mp4"
+        val output = FileOutputStream(File(file))
+
+        val data = ByteArray(1024)
+        val len = input!!.available()
+        var total = 0
+        var count: Int
+        val handler = Handler()
+        dialog.progress_text.text = "$total%"
+
+        //Use coroutine for exporting
+        val exporting = GlobalScope.launch {
+            count = try {
+                input.read(data)
+            } catch (e: Exception) {
+                -1
+            }
+            while (count != -1) {
+                total += count
+
+                handler.post {
+                    dialog.progress_text.text =
+                        (total.toDouble() / len * 100).toInt().toString() + "%"
+                }
+
+                try {
+                    output.write(data, 0, count)
+                    count = input.read(data)
+                } catch (e: Exception) {
+                    checkStop = true
+                    break
+                }
+            }
+
+            if (!checkStop) {
+                input.close()
+                output.flush()
+                output.close()
+                dialog.dismiss()
+
+                handler.post {
+                    val toast = Toast(this@ExportActivity)
+                    val layout = layoutInflater.inflate(R.layout.finish_toast, null)
+                    toast.setGravity(Gravity.CENTER, 0, 0)
+                    toast.view = layout
+                    toast.show()
+                }
+            }
+        }
+
+        dialog.export_stop.setOnClickListener {
+            input.close()
+            output.close()
+            dialog.dismiss()
+            File(file).delete()
+            exporting.cancel()
         }
     }
 
