795fc3a3d2fc5cf05220e89f5bab85a34b52ed79
kdh0324 <kdh0324@postech.ac.kr>
2020-07-22 16:53:26 +0900

    feat : Effect Class
    draw function을 제외하고 구현

---
+++ app/src/main/java/com/teamisland/zzazz/utils/Effect.kt
@@ -0,0 +1,158 @@
+package com.teamisland.zzazz.utils
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.annotation.FloatRange
+
+/**
+ * Class for each effects
+ */
+class Effect(
+    private var startFrame: Int,
+    private var endFrame: Int,
+    private val type: Int,
+    private var color: Int,
+    bitmapArrayList: ArrayList<Bitmap>,
+    pointArrayList: ArrayList<Point>,
+    widthArrayList: ArrayList<Float>,
+    heightArrayList: ArrayList<Float>
+) {
+    private var dataArrayList: ArrayList<Data> = ArrayList(endFrame - startFrame + 1)
+
+    init {
+        for (frame in 0 until endFrame) {
+            dataArrayList[frame] = Data(
+                bitmapArrayList[frame],
+                pointArrayList[frame],
+                widthArrayList[frame],
+                heightArrayList[frame]
+            )
+        }
+    }
+
+    /**
+     * @param color to set color
+     */
+    fun setColor(color: Int) {
+        this.color = color
+    }
+
+    /**
+     * @param startFrame to set startFrame
+     */
+    fun setStart(startFrame: Int) {
+        this.startFrame = startFrame
+    }
+
+    /**
+     * @param endFrame to set endFrame
+     */
+    fun setEnd(endFrame: Int) {
+        this.endFrame = endFrame
+    }
+
+    /**
+     * Move x coordinate at frame.
+     */
+    fun moveX(frame: Int, dx: Int) {
+        dataArrayList[frame - startFrame].moveX(dx)
+    }
+
+    /**
+     * Move y coordinate at frame.
+     */
+    fun moveY(frame: Int, dy: Int) {
+        dataArrayList[frame - startFrame].moveY(dy)
+    }
+
+    /**
+     * Rotate the effect at frame.
+     */
+    fun rotate(frame: Int, @FloatRange(from = 0.0, to = 360.0) degree: Float) {
+        dataArrayList[frame - startFrame].rotate(degree)
+    }
+
+    /**
+     * Change width of effect at frame.
+     */
+    fun changeWidth(frame: Int, dw: Float) {
+        dataArrayList[frame - startFrame].changeWidth(dw)
+    }
+
+    /**
+     * Change height of effect at frame.
+     */
+    fun changeHeight(frame: Int, dh: Float) {
+        dataArrayList[frame - startFrame].changeHeight(dh)
+    }
+
+    /**
+     * Draw effect with canvas.
+     */
+    fun draw(frame: Int, canvas: Canvas) {
+        TODO("have to implement")
+    }
+
+    /**
+     * Data of effect on each frames.
+     */
+    private class Data(
+        private var bitmap: Bitmap,
+        private var point: Point,
+        private var width: Float,
+        private var height: Float
+    ) {
+
+        private var degree: Float = 0F
+
+        /**
+         * Move x coordinate.
+         */
+        fun moveX(dx: Int) {
+            point.x += dx
+        }
+
+        /**
+         * Move y coordinate.
+         */
+        fun moveY(dy: Int) {
+            point.y += dy
+        }
+
+        /**
+         * Rotate effect Image.
+         */
+        fun rotate(@FloatRange(from = 0.0, to = 360.0) angle: Float) {
+            degree += angle
+            degree %= 360
+        }
+
+        /**
+         * Change width of effect.
+         */
+        fun changeWidth(dw: Float) {
+            width += dw
+        }
+
+        /**
+         * Change height of effect.
+         */
+        fun changeHeight(dh: Float) {
+            height += dh
+        }
+    }
+
+    /**
+     * Coordinate of point
+     */
+    class Point(
+        /**
+         * x point
+         */
+        var x: Int,
+        /**
+         * y point
+         */
+        var y: Int
+    )
+}
