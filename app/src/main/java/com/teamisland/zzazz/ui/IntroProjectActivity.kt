d4022c48023780703fbb912de8b9ef5d3c166a3d
kdh0324 <55946729+kdh0324@users.noreply.github.com>
2020-07-24 16:48:25 +0900

    feat : IntroActivity 틀 (#59)
    필요없는 drawable 제거
    IntroActivity를 swipe형으로 구현

---
+++ app/src/main/java/com/teamisland/zzazz/ui/IntroProjectActivity.kt
@@ -0,0 +1,26 @@
+package com.teamisland.zzazz.ui
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.teamisland.zzazz.R
+
+/**
+ * Show saved project
+ */
+class IntroProjectActivity : Fragment() {
+
+    /**
+     * Override [Fragment.onCreateView]
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.activity_intro_project, container, false)
+    }
+}
