90e2fafea2f2722d479252fd9fd882a080400248
kdh0324 <kdh0324@postech.ac.kr>
2020-08-22 13:51:16 +0900

    feat : Fix Intro Activity UI
    TODO: Random Text Position

--- app/src/main/java/com/teamisland/zzazz/ui/IntroActivity.kt
+++ app/src/main/java/com/teamisland/zzazz/ui/IntroActivity.kt
@@ -3,18 +3,19 @@ package com.teamisland.zzazz.ui
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import com.teamisland.zzazz.R
-import com.teamisland.zzazz.utils.GetVideoData
+import com.teamisland.zzazz.utils.UnitConverter.float2DP
 import kotlinx.android.synthetic.main.activity_intro.*
-import java.io.File
+import java.util.*
 
 /**
  * Main activity of Intro Activity
@@ -31,6 +32,9 @@ class IntroActivity : AppCompatActivity() {
 
         private const val TAKE_VIDEO = 2
     }
+
+    private lateinit var circle: ImageView
+    private lateinit var underline: ImageView
 
     @Suppress("SameParameterValue")
     private fun getVideo(requestCode: Int) {
@@ -55,6 +59,54 @@ class IntroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_intro)
         window.navigationBarColor = getColor(R.color.Background)
 
+        val random = Random()
+        val textArray = arrayOf(
+            getString(R.string.intro1),
+            getString(R.string.intro2),
+            getString(R.string.intro3),
+            getString(R.string.intro4),
+            getString(R.string.intro5)
+        )
+        val index = random.nextInt(textArray.size - 1)
+        random_text.text = textArray[index]
+        random_text.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40f)
+
+        circle = ImageView(this).apply {
+            setImageResource(R.drawable.circle_point)
+            id = View.generateViewId()
+        }
+        underline = ImageView(this).apply {
+            setImageResource(R.drawable.underline)
+            id = View.generateViewId()
+        }
+        constraintLayout.apply {
+            addView(circle)
+            addView(underline)
+        }
+
+        when (index) {
+            0 -> {
+                setPosition(circle.id, 132, 132)
+                setPosition(underline.id, 16, 44)
+            }
+            1 -> {
+                setPosition(circle.id, 40, 88)
+                setPosition(underline.id, 16, 44)
+            }
+            2 -> {
+                setPosition(circle.id, 152, 132)
+                setPosition(underline.id, 16, 44)
+            }
+            3 -> {
+                setPosition(circle.id, 16, 132)
+                setPosition(underline.id, 16, 44)
+            }
+            4 -> {
+                setPosition(circle.id, 178, 132)
+                setPosition(underline.id, 64, 44)
+            }
+        }
+
         val shrink = AnimationUtils.loadAnimation(this, R.anim.shrink)
         zzazz.setOnTouchListener { v, event ->
             when (event.action) {
@@ -75,6 +127,35 @@ class IntroActivity : AppCompatActivity() {
 
         val bounce = AnimationUtils.loadAnimation(this, R.anim.bounce)
         linearLayout.startAnimation(bounce)
+    }
+
+    private fun setPosition(id: Int, startMargin: Int, bottomMargin: Int) {
+        val bias =
+            if (id == circle.id) 1 - (bottomMargin + 24) / 220f
+            else 1 - (bottomMargin + 4) / 220f
+        ConstraintSet().apply {
+            clone(constraintLayout)
+            connect(
+                id,
+                ConstraintSet.START,
+                constraintLayout.id,
+                ConstraintSet.START,
+                float2DP(startMargin.toFloat(), resources).toInt()
+            )
+            connect(
+                id,
+                ConstraintSet.TOP,
+                constraintLayout.id,
+                ConstraintSet.TOP
+            )
+            connect(
+                id,
+                ConstraintSet.BOTTOM,
+                linearLayout.id,
+                ConstraintSet.TOP
+            )
+            setVerticalBias(id, bias)
+        }.applyTo(constraintLayout)
     }
 
     /**
